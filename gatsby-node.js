bbdd082e1b26768ca45561c438a0a01aabf37fff
Ben Robertson <hi@benrobertson.io>
2020-02-26 10:39:52 -0500

    Test static folder.

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,30 +8,30 @@ const path = require('path');
 const babel = require('@babel/core');
 const fs = require('fs');
 
-exports.onPostBuild = () => {
-  const srcLocation = `${__dirname}/src/functions`;
-  const outputLocation = `${__dirname}/public/functions`;
+// exports.onPostBuild = () => {
+//   const srcLocation = `${__dirname}/src/functions`;
+//   const outputLocation = `${__dirname}/public/functions`;
 
-  const modules = glob.sync('*.js', { cwd: srcLocation });
-  modules.forEach(src => {
-    const moduleSrc = path.join(srcLocation, src);
-    const moduleOut = path.join(outputLocation, path.basename(src, path.extname(src)) + '.js');
+//   const modules = glob.sync('*.js', { cwd: srcLocation });
+//   modules.forEach(src => {
+//     const moduleSrc = path.join(srcLocation, src);
+//     const moduleOut = path.join(outputLocation, path.basename(src, path.extname(src)) + '.js');
 
-    console.log('Compile module: ', moduleSrc);
-    const out = babel.transformFileSync(moduleSrc, {
-      babelrc: true,
-      babelrcRoots: srcLocation,
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              node: '8.10',
-            },
-          },
-        ]
-      ],
-    });
-    fs.writeFileSync(moduleOut, out.code);
-  });
-};
+//     console.log('Compile module: ', moduleSrc);
+//     const out = babel.transformFileSync(moduleSrc, {
+//       babelrc: true,
+//       babelrcRoots: srcLocation,
+//       presets: [
+//         [
+//           '@babel/preset-env',
+//           {
+//             targets: {
+//               node: '8.10',
+//             },
+//           },
+//         ]
+//       ],
+//     });
+//     fs.writeFileSync(moduleOut, out.code);
+//   });
+// };
