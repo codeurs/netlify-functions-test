299286f8ef6f40c83e900aa96d8d3a2242877473
Ben Robertson <hi@benrobertson.io>
2020-02-26 10:52:30 -0500

    Add an example for a custom directory.

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,33 +5,24 @@
  */
 const glob = require('glob');
 const path = require('path');
-const babel = require('@babel/core');
 const fs = require('fs');
 
-// exports.onPostBuild = () => {
-//   const srcLocation = `${__dirname}/src/functions`;
-//   const outputLocation = `${__dirname}/public/functions`;
+exports.onPostBuild = () => {
+  // Configure where the functions are kept and where we want to move them.
+  const srcLocation = `${__dirname}/src/functions`;
+  const outputLocation = `${__dirname}/public/functions`;
 
-//   const modules = glob.sync('*.js', { cwd: srcLocation });
-//   modules.forEach(src => {
-//     const moduleSrc = path.join(srcLocation, src);
-//     const moduleOut = path.join(outputLocation, path.basename(src, path.extname(src)) + '.js');
+  // Get all the functions.
+  const modules = glob.sync('*.js', { cwd: srcLocation });
+  modules.forEach(src => {
+    const moduleSrc = path.join(srcLocation, src);
+    const moduleOut = path.join(outputLocation, path.basename(src, path.extname(src)) + '.js');
 
-//     console.log('Compile module: ', moduleSrc);
-//     const out = babel.transformFileSync(moduleSrc, {
-//       babelrc: true,
-//       babelrcRoots: srcLocation,
-//       presets: [
-//         [
-//           '@babel/preset-env',
-//           {
-//             targets: {
-//               node: '8.10',
-//             },
-//           },
-//         ]
-//       ],
-//     });
-//     fs.writeFileSync(moduleOut, out.code);
-//   });
-// };
+    // Copy file to new location.
+    fs.copyFile(moduleSrc, moduleOut, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  });
+};
---
+++ src/functions/custom-directory.js
@@ -0,0 +1,6 @@
+exports.handler = async function (event) {
+  return {
+    statusCode: 200,
+    body: 'This function is in a custom directory'
+  };
+}
